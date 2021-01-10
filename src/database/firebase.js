694d6c4c31acc5ddec91c1198f31fcef42e1f844
thannathornyuw <thannathornyuw@gosoft.co.th>
2021-01-10 13:09:17 +0700

    add database

---
+++ src/database/firebase.js
@@ -0,0 +1,37 @@
+import { firebase } from "firebase/firebase-app";
+import "firebase/firebase-firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYuQjPGDvmWfG1tgFQqT4VUH5DzCd3eHY",
+    authDomain: "orangejin-e782e.firebaseapp.com",
+    projectId: "orangejin-e782e",
+    storageBucket: "orangejin-e782e.appspot.com",
+    messagingSenderId: "1076004058618",
+    appId: "1:1076004058618:web:c149927e97046e131d6043",
+    measurementId: "G-TVS2V9FZPN"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.fireStore();
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyBYuQjPGDvmWfG1tgFQqT4VUH5DzCd3eHY",
+//     authDomain: "orangejin-e782e.firebaseapp.com",
+//     projectId: "orangejin-e782e",
+//     storageBucket: "orangejin-e782e.appspot.com",
+//     messagingSenderId: "1076004058618",
+//     appId: "1:1076004058618:web:c149927e97046e131d6043",
+//     measurementId: "G-TVS2V9FZPN"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
